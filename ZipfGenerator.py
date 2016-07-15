3de3709e19c6080b5cd5a6c81b546a208978909b
Utsav Drolia <utsavdrolia@gmail.com>
2016-07-15 17:53:52 -0400

    Zipf generator

--- ZipfGenerator.py
+++ ZipfGenerator.py
@@ -2,6 +2,9 @@ import random
 import bisect
 import math
 
+import numpy
+
+
 class ZipfGenerator:
 
     def __init__(self, n, alpha):
@@ -18,3 +21,17 @@ class ZipfGenerator:
 
         # Translate the Zipf variable:
         return bisect.bisect(self.distMap, u) - 1
+
+
+def randZipf(n, alpha, numSamples):
+    # Calculate Zeta values from 1 to n:
+    tmp = numpy.power( numpy.arange(1, n+1), -alpha )
+    zeta = numpy.r_[0.0, numpy.cumsum(tmp)]
+    # Store the translation map:
+    distMap = [x / zeta[-1] for x in zeta]
+    # Generate an array of uniform 0-1 pseudo-random values:
+    u = numpy.random.random(numSamples)
+    # bisect them with distMap
+    v = numpy.searchsorted(distMap, u)
+    samples = [t-1 for t in v]
+    return samples
